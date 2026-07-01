6c77dc1b166f386f3cc40dd6e21d1388957d926a
Anuja Shukla <anujashukla521@gmail.com>
2026-07-01 19:49:06 +0530

    Linear search implementation

---
+++ 100-Days-DSA-Revision/Day21/linear-search-last-occurrence.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lastOccurrence(const vector<int> &arr, int target)
+{
+    int index = -1;
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        if (target == arr[i])
+        {
+           index = i;
+        }
+    }
+    return index;
+}
+int main()
+{
+    vector<int> arr = {2,7,8,1,4,2,6,4,2,1};
+    int target;
+    cout << "Enter element to search: ";
+    cin >> target;
+    int index = lastOccurrence(arr, target);
+    if (index != -1){
+        cout << "Last occurrence at index " << index;
+    }
+    else
+    {
+        cout << "Element not found";
+    }
+    return 0;
+}
